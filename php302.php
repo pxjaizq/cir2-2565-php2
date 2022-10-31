422cc4b9d851c44bde419b762d63b5f669691ebb
pxjaizq <pojai71688@gmail.com>
2022-10-31 12:10:24 +0700

    first commit

---
+++ php302.php
@@ -0,0 +1,14 @@
+<?php
+ $message = "สวัสดีชาวนาแม็กซ์";
+ $fname = "โงกุน";
+ $age = 22;
+ $son = 2;
+ $image = '<img src="https://www.webcomicsweekend.com/wp-content/uploads/2016/10/Sokokung.jpg" alt="z">';
+ echo "<h1>$message</h1>";
+ echo "<hr />";
+ echo "<h2>My name is $fname </h2>";
+ echo "<p>My age is $age</p>";
+ echo "My son have $son คน";
+ echo "<br />";
+ echo $image;
+?>
---
+++ php303.php
@@ -0,0 +1,18 @@
+<?php
+  $star = "Kwangya star";
+  
+  function Exo(){
+    echo "สวัสดีชาว $star";
+    $star = "exoplanet";
+    
+    echo"1.Local veriable $star";
+    echo"<br />";
+    echo"3.Static variable $x";
+    echo"<br / >"
+
+  }
+  Exo();
+  echo $star;
+?>
+  
+  
